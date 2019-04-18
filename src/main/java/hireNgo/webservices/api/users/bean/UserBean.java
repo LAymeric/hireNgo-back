a40614da936ee42cd77a3f7ca79f457f57a7805e
hermancedornier <hdornier@reeliant.com>
2019-04-18 20:50:26 +0200

    userBean colle avec le front

--- src/main/java/hireNgo/webservices/api/users/bean/UserBean.java
+++ src/main/java/hireNgo/webservices/api/users/bean/UserBean.java
@@ -6,7 +6,10 @@ import lombok.Setter;
 @Getter // annotation permettant de ne pas avoir à ecrire nos getter
 @Setter // et nos setters : plus clair et lisible
 public class UserBean {
-    private String firstname;
     private String lastname;
+    private String firstname;
+    private String pwd;
     private String email;
+    private String phone;
+    private String birthday;
 }
